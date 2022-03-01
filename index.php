44431e32a7feea16adad63104616c61428f3fe0b
SamuelVnuk <73060091+SamuelVnuk@users.noreply.github.com>
2022-03-01 10:35:26 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <meta hhtp-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>    
+
+    <body>
+        <a href="/pages/register.php">Registracia</a>
+        <a href="/pages/login.php">Login</a>
+    </body>
+</html>    
